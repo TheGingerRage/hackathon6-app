e7acb2e18f986fe523e3a4315b6c58b34a9f36ad
Tracy Bragdon <tracy.bragdon@ncino.com>
2019-10-29 10:30:19 -0400

    fix merge issues with navigation

--- app/src/main/java/com/example/hackathon6_app/MainActivity.java
+++ app/src/main/java/com/example/hackathon6_app/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home,R.id.nav_qr, R.id.nav_profile_creator, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_speakers, R.id.nav_profile, R.id.nav_share, R.id.nav_send)
+                R.id.nav_tools, R.id.nav_speakers, R.id.nav_profile, R.id.nav_share)
                 .setDrawerLayout(drawer)
                 .build();
         navigationView.setItemIconTintList(null);
--- app/src/main/java/com/example/hackathon6_app/NavigationActivity.java
+++ app/src/main/java/com/example/hackathon6_app/NavigationActivity.java
@@ -29,14 +29,6 @@ public abstract class NavigationActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -44,7 +36,7 @@ public abstract class NavigationActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_tools, R.id.nav_share)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_profile_fragment);
