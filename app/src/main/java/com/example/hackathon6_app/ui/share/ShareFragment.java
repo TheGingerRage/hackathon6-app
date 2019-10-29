fcece8f058e59e1d348dc93037d77e0b9e027f3d
Stephen Middaugh <stephen.middaugh@ncino.com>
2019-10-29 11:17:37 -0400

    remove duplicate code in the share fragment and move profile creator to the demo section

--- app/src/main/java/com/example/hackathon6_app/ui/share/ShareFragment.java
+++ app/src/main/java/com/example/hackathon6_app/ui/share/ShareFragment.java
@@ -1,24 +1,16 @@
 package com.example.hackathon6_app.ui.share;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.hackathon6_app.R;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.integration.android.IntentIntegrator;
 
 public class ShareFragment extends Fragment {
     public static final int QRcodeWidth = 350 ;
@@ -31,67 +23,6 @@ public class ShareFragment extends Fragment {
                 ViewModelProviders.of(this).get(ShareViewModel.class);
         View root = inflater.inflate(R.layout.fragment_share, container, false);
 
-
-        ImageView qrCodeImg = root.findViewById(R.id.qrCodeImage);
-        ScanButton = root.findViewById(R.id.scanButton);
-
-        final String profileText = "stephen.middaugh@ncino.com::4234831361::@stephenm::I am a software developer in Wilmington, NC.";
-
-        try{
-            Bitmap bitmap = TextToImageEncode(profileText);
-            qrCodeImg.setImageBitmap(bitmap);
-        }
-        catch(WriterException e){
-
-        }
-
-        ScanButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                IntentIntegrator integrator = new IntentIntegrator(getActivity());
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
-                integrator.setPrompt("Scan");
-                integrator.setCameraId(0);
-                integrator.setBeepEnabled(true);
-                integrator.setBarcodeImageEnabled(true);
-                integrator.initiateScan();
-            }
-        });
-
         return root;
-    }
-
-    Bitmap TextToImageEncode(String Value) throws WriterException {
-        BitMatrix bitMatrix;
-        try {
-            bitMatrix = new MultiFormatWriter().encode(
-                    Value,
-                    BarcodeFormat.DATA_MATRIX.QR_CODE,
-                    QRcodeWidth, QRcodeWidth, null
-            );
-
-        } catch (IllegalArgumentException Illegalargumentexception) {
-
-            return null;
-        }
-        int bitMatrixWidth = bitMatrix.getWidth();
-
-        int bitMatrixHeight = bitMatrix.getHeight();
-
-        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
-
-        for (int y = 0; y < bitMatrixHeight; y++) {
-            int offset = y * bitMatrixWidth;
-
-            for (int x = 0; x < bitMatrixWidth; x++) {
-
-                pixels[offset + x] = bitMatrix.get(x, y) ?
-                        getResources().getColor(R.color.QRCodeBlackColor):getResources().getColor(R.color.QRCodeWhiteColor);
-            }
-        }
-        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
-
-        bitmap.setPixels(pixels, 0, 350, 0, 0, bitMatrixWidth, bitMatrixHeight);
-        return bitmap;
     }
 }
